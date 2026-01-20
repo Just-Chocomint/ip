6f329928286d8a47f907638dc63deff21d72a13a
JustChocomint <58057821+Just-Chocomint@users.noreply.github.com>
2026-01-20 17:40:44 +0800

    Rename bot and add greeting/exit messages

---
+++ src/main/java/Fern.java
@@ -0,0 +1,14 @@
+public class Fern {
+    public static void main(String[] args) {
+        String name = ",------.  \n"
+                + "|  .---',---. ,--.--.,--,--,  \n"
+                + "|  `--,| .-. :|  .--'|      \\ \n"
+                + "|  |`  \\   --.|  |   |  ||  | \n"
+                + "`--'    `----'`--'   `--''--' \n";
+        String line = "________________________________________";
+        System.out.println(line);
+        System.out.println("Hii i am\n" + name + "How can i help you?\n" + line);
+        System.out.println("Byebye");
+        System.out.println(line);
+    }
+}
