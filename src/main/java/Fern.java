16b97e9d2af9017a716402d2591ddf4fc747995e
JustChocomint <58057821+Just-Chocomint@users.noreply.github.com>
2026-01-20 20:31:14 +0800

    Add Echo

--- src/main/java/Fern.java
+++ src/main/java/Fern.java
@@ -1,3 +1,7 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 public class Fern {
     public static void main(String[] args) {
         String name = ",------.  \n"
@@ -5,10 +9,21 @@ public class Fern {
                 + "|  `--,| .-. :|  .--'|      \\ \n"
                 + "|  |`  \\   --.|  |   |  ||  | \n"
                 + "`--'    `----'`--'   `--''--' \n";
-        String line = "________________________________________";
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy hh.mm a");
+        String line = "--------------------"
+                + LocalDateTime.now().format(timeFormatter)
+                + "---------------------";
         System.out.println(line);
-        System.out.println("Hii i am\n" + name + "How can i help you?\n" + line);
+        System.out.println("Hii, i am\n" + name + "Whats up?\n" );
+        Scanner scanner = new Scanner(System.in);
+        String userInput = "";
+        while(!userInput.equals("bye")) {
+            System.out.print("You: ");
+            userInput = scanner.nextLine();
+            System.out.println("Fern: " + userInput);
+        }
         System.out.println("Byebye");
         System.out.println(line);
+        scanner.close();
     }
 }
