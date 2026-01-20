3dbb2ae6bd4a4848da4d09e4c0a602c45cad0b5f
JustChocomint <58057821+Just-Chocomint@users.noreply.github.com>
2026-01-20 21:06:34 +0800

    Add ability to read/write to list

--- src/main/java/Fern.java
+++ src/main/java/Fern.java
@@ -9,21 +9,41 @@ public class Fern {
                 + "|  `--,| .-. :|  .--'|      \\ \n"
                 + "|  |`  \\   --.|  |   |  ||  | \n"
                 + "`--'    `----'`--'   `--''--' \n";
+
         DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy hh.mm a");
         String line = "--------------------"
                 + LocalDateTime.now().format(timeFormatter)
                 + "---------------------";
+
+        String[] storage = new String[100];
+        int counter = 0;
+
         System.out.println(line);
         System.out.println("Hii, i am\n" + name + "Whats up?\n" );
         Scanner scanner = new Scanner(System.in);
         String userInput = "";
-        while(!userInput.equals("bye")) {
+        while(true) {
             System.out.print("You: ");
             userInput = scanner.nextLine();
-            System.out.println("Fern: " + userInput);
+            if (userInput.equalsIgnoreCase("bye")){
+                break;
+            } else if (userInput.equalsIgnoreCase("list")) {
+                printList(storage, counter);
+            } else {
+                storage[counter] = userInput;
+                System.out.println("Fern: Added item (" + userInput + ")");
+                if (++counter == 100) {
+                    counter = 0;
+                }
+            }
         }
-        System.out.println("Byebye");
+        System.out.println("Fern: Byebye~~");
         System.out.println(line);
         scanner.close();
+    }
+    private static void printList(String[] list,int count) {
+        for(int i = 0; i < count; i++){
+            System.out.println(i + 1 + ". " + list[i]);
+        }
     }
 }
