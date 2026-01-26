edcf227a87d02bc31442e02331babd5ba9089d94
JustChocomint <58057821+Just-Chocomint@users.noreply.github.com>
2026-01-27 01:19:10 +0800

    Move command handling logic from Fern.java to Commands.java

--- src/main/java/Commands.java
+++ src/main/java/Commands.java
@@ -1,7 +1,125 @@
 public class Commands {
     private final TaskList tasks;
+    private String[] userInputSplit;
+    private String userInput;
+
     public Commands(TaskList taskList){
         this.tasks = taskList;
     }
 
+    public void handle(String userInput) throws FernException{
+        this.userInput = userInput;
+        this.userInputSplit = userInput.split(" ");
+        String firstWord = userInputSplit[0].toLowerCase();
+        switch (firstWord) {
+        case "mark":
+        case "unmark":
+            handleMark();
+            break;
+        case "todo":
+            handleToDo();
+            break;
+        case "deadline":
+            handleDeadline();
+            break;
+        case "event":
+            handleEvent();
+            break;
+        case "delete":
+            handleDelete();
+            break;
+        default:
+            throw new UnkownCommandException();
+        }
+    }
+
+    /**
+     * Handle marking/unmarking tasks
+     **/
+    private void handleMark() throws FernException{
+        if (userInputSplit.length < 2) {
+            throw new IncompleteCommandException("Task nunmber");
+        }
+        int markIdx = checkTaskExist();
+        tasks.get(markIdx).toggleCompletion();
+        String completion = tasks.get(markIdx).isCompleted ? "marked" : "unmarked";
+        UI.say("(" + tasks.get(markIdx).getDescription() + ") " + completion);
+    }
+
+    /**
+     * Handle checking and adding todo tasks
+     **/
+    private void handleToDo() throws FernException{
+        String taskDescription = userInput.substring(4).trim();
+        if (taskDescription.isEmpty()) {
+            throw new IncompleteCommandException("Description");
+        }
+        tasks.add(new ToDo(taskDescription));
+        UI.say("Added ToDo (" + taskDescription + ")");
+    }
+
+    /**
+     * Handle checking and adding Deadline tasks
+     **/
+    private void handleDeadline() throws FernException{
+        int startOfDate = userInput.indexOf("/by");
+        if (startOfDate > 0) {
+            String taskDescription = userInput.substring(8, startOfDate).trim();
+            String by = userInput.substring(startOfDate + 4);
+            tasks.add(new Deadline(taskDescription, by));
+            UI.say("Added Deadline (" + taskDescription + ")");
+        } else {
+            throw new IncompleteCommandException("/by");
+        }
+    }
+
+    /**
+     * Handle checking and adding Event tasks
+     **/
+    private void handleEvent() throws FernException{
+        int startOfFrom = userInput.indexOf("/from");
+        int startOfTo = userInput.indexOf("/to");
+        if (startOfFrom > 0 && startOfTo > 0) {
+            String taskDescription = userInput.substring(5, startOfFrom).trim();
+            String from = userInput.substring(startOfFrom + 6, startOfTo);
+            String to = userInput.substring(startOfTo + 4);
+            tasks.add(new Event(taskDescription, from, to));
+            UI.say("Added Event (" + taskDescription + ")");
+        } else {
+            throw new IncompleteCommandException("/from and /to");
+        }
+    }
+
+    /**
+     * Handle deleting tasks
+     **/
+    private void handleDelete() throws FernException{
+        if (userInputSplit.length < 2) {
+            throw new IncompleteCommandException("Task nunmber");
+        }
+        int deleteIdx = checkTaskExist();
+        String deletedText = tasks.get(deleteIdx).toString();
+        tasks.remove(deleteIdx);
+        UI.say("(" + deletedText + ") deleted. Left " + tasks.size() + " Tasks");
+    }
+
+    /**
+     * Check that task exists
+     * Return index if there is no problem
+     **/
+    private int checkTaskExist() throws FernException{
+        try { // try block to parse task number from string to int
+            int idx = Integer.parseInt(userInputSplit[1]) - 1;
+            if (idx >= tasks.size() || idx < 0) {
+                throw new FernException("Fern: that task no exist");
+            } else if (userInputSplit[0].equals("mark") && tasks.get(idx).getCompletion()) {
+                throw new FernException("Fern: u alr completed");
+            } else if (userInputSplit[0].equals("unmark") && !tasks.get(idx).getCompletion()) {
+                throw new FernException("Fern: it was alr unmarked bruh");
+            }
+            return idx;
+        } catch (NumberFormatException e) {
+            throw new FernException("Fern: what task is that");
+        }
+    }
 }
--- src/main/java/Fern.java
+++ src/main/java/Fern.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Fern {
     private static final TaskList tasks = new TaskList();
+    private static final Commands commands = new Commands(tasks);
     /**
      * Starts the chatbot
      */
@@ -12,9 +13,6 @@ public class Fern {
         while(true) {
             System.out.print("You: \n");
             String userInput = scanner.nextLine().trim();
-            String[] userInputSplit = userInput.split(" ");
-            String firstWord = userInputSplit[0].toLowerCase();
-
             if (userInput.equalsIgnoreCase("bye")){
                 break;
             } else if (userInput.equalsIgnoreCase("list")) {
@@ -22,63 +20,7 @@ public class Fern {
                 continue;
             }
             try {
-                String taskDescription = "";
-                switch (firstWord) {
-                    case "mark":
-                    case "unmark":
-                        if (userInputSplit.length < 2) {
-                            throw new IncompleteCommandException("Task nunmber");
-                        }
-                        int markIdx = checkTaskExist(userInputSplit);
-                        tasks.get(markIdx).toggleCompletion();
-                        String completion = tasks.get(markIdx).isCompleted ? "marked" : "unmarked";
-                        UI.say("(" + tasks.get(markIdx).getDescription() + ") " + completion);
-
-                        break;
-                    case "todo":
-                        taskDescription = userInput.substring(4).trim();
-                        if (taskDescription.isEmpty()) {
-                            throw new IncompleteCommandException("Description");
-                        }
-                        tasks.add(new ToDo(taskDescription));
-                        UI.say("Added ToDo (" + taskDescription + ")");
-                        break;
-                    case "deadline":
-                        int startOfDate = userInput.indexOf("/by");
-                        if (startOfDate > 0) {
-                            taskDescription = userInput.substring(8, startOfDate).trim();
-                            String by = userInput.substring(startOfDate + 4);
-                            tasks.add(new Deadline(taskDescription, by));
-                            UI.say("Added Deadline (" + taskDescription + ")");
-                        } else {
-                            throw new IncompleteCommandException("/by");
-                        }
-                        break;
-                    case "event":
-                        int startOfFrom = userInput.indexOf("/from");
-                        int startOfTo = userInput.indexOf("/to");
-                        if (startOfFrom > 0 && startOfTo > 0) {
-                            taskDescription = userInput.substring(5, startOfFrom).trim();
-                            String from = userInput.substring(startOfFrom + 6, startOfTo);
-                            String to = userInput.substring(startOfTo + 4);
-                            tasks.add(new Event(taskDescription, from, to));
-                            UI.say("Added Event (" + taskDescription + ")");
-                        } else {
-                            throw new IncompleteCommandException("/from and /to");
-                        }
-                        break;
-                    case "delete":
-                        if (userInputSplit.length < 2) {
-                            throw new IncompleteCommandException("Task nunmber");
-                        }
-                        int deleteIdx = checkTaskExist(userInputSplit);
-                        String deletedText = tasks.get(deleteIdx).toString();
-                        tasks.remove(deleteIdx);
-                        UI.say("(" + deletedText + ") deleted. Left " + tasks.size() + " Tasks");
-                        break;
-                    default:
-                        throw new UnkownCommandException();
-                }
+                commands.handle(userInput);
             } catch (FernException e){
                 UI.say(e.getMessage());
             }
@@ -87,32 +29,4 @@ public class Fern {
         UI.end();
         scanner.close();
     }
-
-
-    /**
-     * Check that task exists
-     * Return index if there is no problem
-     **/
-    private static int checkTaskExist(String[] userInputSplit) throws FernException{
-        try { // try block to parse task number from string to int
-            int idx = Integer.parseInt(userInputSplit[1]) - 1;
-            if (idx >= tasks.size() || idx < 0) {
-                throw new FernException("Fern: that task no exist");
-            } else if (userInputSplit[0].equals("mark") && tasks.get(idx).getCompletion()) {
-                throw new FernException("Fern: u alr completed");
-            } else if (userInputSplit[0].equals("unmark") && !tasks.get(idx).getCompletion()) {
-                throw new FernException("Fern: it was alr unmarked bruh");
-            }
-            return idx;
-        } catch (NumberFormatException e) {
-            throw new FernException("Fern: what task is that");
-        }
-    }
-
-    /**
-     * Delete task from list
-     **/
-
-
-
 }
